8973a438ac91bf814dc591593710b09bab29a686
rlrc40 <raul.rodriguez.carracedo@alumnos.uva.es>
2017-05-09 15:27:31 +0200

    Button create group

---
+++ src/app/components/my-profile/profile.component.ts
@@ -0,0 +1,48 @@
+import 'rxjs/add/operator/switchMap';
+import { Component,  OnInit }       from '@angular/core';
+import { ActivatedRoute, Params, Router }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { User }         from '../../../User';
+import { UserService } from '../../_services/user.service';
+@Component({
+  selector: 'my-profile',
+  templateUrl: './my-profile.component.html',
+})
+
+export class MyProfileComponent implements OnInit{
+  user: User;
+  currentUser: User;
+  connectedUser: User;
+  users: User[] = [];
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        console.log(this.currentUser);};
+        
+  
+  ngOnInit() {
+        this.loadAllUsers();this.userService.getById(this.currentUser["_id"]).subscribe(connectedUser => { this.connectedUser = connectedUser; });
+        console.log(this.connectedUser);
+        this.route.params
+        .switchMap((params: Params) => this.userService.getById(params["id"]))
+        .subscribe(user => this.user = user);
+
+        
+    }
+ 
+
+  private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+  gotoEditProfile(): void {
+    const id = "_id";
+    this.router.navigate(['/template/edit-profile', this.user[id]]);
+  }
+}
